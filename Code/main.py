1884efaedb1f3f0d2e01484a2daf4b5529cbe0ff
Ganesh Jainarain <RicheyJay2@gmail.com>
2022-02-22 18:58:05 -0500

    installed packages, started to get some data using pandas Data Reader module, worked fine will update to make this more dynamic later on , see Readme for progress update and terminal outputs

--- Code/main.py
+++ Code/main.py
@@ -2,3 +2,10 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import pandas_datareader as data 
+
+
+start = '2010-01-01'
+end = '2019-12-31'
+#will make this more dynamic but for now we are testing getting data
+df = data.DataReader('AAPL', 'yahoo', start, end)
+print(df.head())
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # A Stock Trend Prediction Web Application in Python. Here we will use Streamlit, an open-source Python library, that makes it easy to build custom web apps for Machine Learning and Data Science.
+
+# Step By Step Terminal Testing Output
+
+![Retrieving](Outputs/TestingRetrievalOfData.png)
